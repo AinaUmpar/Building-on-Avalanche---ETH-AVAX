280d3a0889101d9bf8980ce7f9910c695f16d103
AinaUmpar <139347400+AinaUmpar@users.noreply.github.com>
2024-05-29 01:30:37 +0800

    Update Degen.sol

--- Degen.sol
+++ Degen.sol
@@ -5,12 +5,12 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Degen is ERC20, Ownable {
     mapping(uint256 => uint256) public prices;
+    mapping(uint256 => bool) public itemRedeemed;
 
     constructor(address firstOwner)
         ERC20("Degen", "DGN")
         Ownable(firstOwner)
     {
-        _mint(firstOwner, 600);
         prices[1] = 50;
         prices[2] = 100;
         prices[3] = 150;
@@ -21,22 +21,29 @@ contract Degen is ERC20, Ownable {
         _mint(receiver, amount);
     }
 
-    function burn(uint256 amount) public onlyOwner {
-        require(amount > 0 && balanceOf(msg.sender) >= amount);
-        _burn(msg.sender, amount);
-    }
-
     function transfer(address to, uint256 amount) public override returns (bool) {
         _transfer(msg.sender, to, amount);
         return true;
     }
 
     function redeem(uint256 itemId) public {
+        require(itemId >= 1 && itemId <= 3, "Invalid: itemId must be between 1 and 3");
         uint256 price = prices[itemId];
-        _burn(msg.sender, price);
+        require(balanceOf(msg.sender) >= price, "Not enough balance");
+        require(!itemRedeemed[itemId], "This Item already redeemed");
+
+        _transfer(msg.sender, address(this), price); 
+
+        itemRedeemed[itemId] = true; 
+
     }
 
-    function balanceOf(address account) public view override returns (uint256) {
-        return super.balanceOf(account);
+    function burn(uint256 amount) public {
+        require(amount > 0 && balanceOf(msg.sender) >= amount, "Insufficient balance");
+        _burn(msg.sender, amount);
+    }
+
+    function balanceOfAddress(address account) public view returns (uint256) {
+        return balanceOf(account);
     }
 }
